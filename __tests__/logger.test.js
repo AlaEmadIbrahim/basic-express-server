84bf3a85f201b9bb0728df2ec326c4d6f5ba1066
AlaEmadIbrahim <ALA.EMAD.IBRAHIM@GMAIL.COM>
2023-06-07 12:11:58 +0300

    done lab2

---
+++ __tests__/logger.test.js
@@ -0,0 +1,17 @@
+"use strict";
+const logger = require("../src/middleware/logger");
+
+describe("Logger middlewaer test", () => {
+  let consoleSpy;
+  let req = {};
+  let res = {};
+  let next = jest.fn();
+
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, "log");
+  });
+  test("It's Logging req details ", () => {
+    logger(req, res, next);
+    expect(consoleSpy).toHaveBeenCalled();
+  });
+});
